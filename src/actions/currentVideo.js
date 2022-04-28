7fa85af84055838cf62ebaaf66b382d72245ce5b
danika-gray <danika.partridge@gmail.com>
2022-04-28 11:29:38 -0700

    create action objects for currentVideo and videoList files, add redux modules to index.js, save current work

--- src/actions/currentVideo.js
+++ src/actions/currentVideo.js
@@ -1,5 +1,8 @@
 var changeVideo = (video) => ({
   //TODO:  Return some action object to change the currently playing video.
+  type: 'videoListEntry/changeVideoOnClick',
+  payload: 'video'
+
 });
 
 export default changeVideo;
